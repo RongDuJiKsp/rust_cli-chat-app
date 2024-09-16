94106c6c7c36fef858d273cfcc809812a9cfc935
YuQiJaang <rdjksp@gmail.com>
2024-09-16 16:48:39 +0800

    fix 可打印字符处理不对

--- src/util/char.rs
+++ src/util/char.rs
@@ -1,17 +1,8 @@
 use std::ops::BitOr;
-
+use unicode_categories::UnicodeCategories;
 pub fn is_char_printable(char: char) -> bool {
-    let fr = false;
-    fr.bitor(char.is_ascii_lowercase())
-        .bitor(char.is_ascii_uppercase())
-        .bitor(char.is_ascii_digit())
-        .bitor(char == '*')
-        .bitor(char == '?')
-        .bitor(char == '@')
-        .bitor(char == '!')
-        .bitor(char == '#')
-        .bitor(char == '$')
-        .bitor(char == '%')
-        .bitor(char == '&')
-        .bitor(char == '\\')
+    false
+        .bitor(!char.is_other_control())
+        .bitor(!char.is_other_private_use())
+        .bitor(!char.is_other_format())
 }
