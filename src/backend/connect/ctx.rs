643792520db9e614411fbe8526a6458cd9750299
YuQiJaang <rdjksp@gmail.com>
2024-09-18 09:05:15 +0800

    添加连接上下文

--- src/backend/connect/ctx.rs
+++ src/backend/connect/ctx.rs
@@ -1,23 +1,27 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::sync::Arc;
+use tokio::net;
 use tokio::net::TcpStream;
 use tokio::sync::Mutex;
-#[derive(Clone)]
-pub struct SocketConnectCtx {
-    addr: SocketAddr,
-    stream: Arc<Mutex<TcpStream>>,
-}
+
 #[derive(Clone)]
 pub struct ConnCtx {
-    as_server: Arc<Mutex<Vec<SocketConnectCtx>>>,
-    as_client: Arc<Mutex<Vec<SocketConnectCtx>>>,
+    as_server: Arc<Mutex<HashMap<SocketAddr, TcpStream>>>,
+    as_client: Arc<Mutex<HashMap<SocketAddr, TcpStream>>>,
 }
 impl ConnCtx {
     pub fn new() -> ConnCtx {
         ConnCtx {
-            as_server: Arc::new(Mutex::new(Vec::new())),
-            as_client: Arc::new(Mutex::new(Vec::new())),
+            as_server: Arc::new(Mutex::new(HashMap::new())),
+            as_client: Arc::new(Mutex::new(HashMap::new())),
         }
     }
-
+    pub async fn try_conn(&self, addr: &str) -> anyhow::Result<()> {
+        let remote = SocketAddr::from_str(addr)?;
+        let conn = net::TcpStream::connect(&remote).await?;
+        self.as_client.lock().await.insert(remote, conn);
+        Ok(())
+    }
 }
